7d0fa4bacf3ec5998253d0fe777bb0bc300c226f
omegadesigner <omegadesigner@hotmail.com>
2020-10-16 09:56:23 -0400

    Missed One Skill

--- character-sheet-generator/src/components/Skills.jsx
+++ character-sheet-generator/src/components/Skills.jsx
@@ -37,7 +37,7 @@ class Skills extends Component {
                 <h4 className="skills-list">Mechanic<h2 className="skills-list-numbers">{Math.floor((this.props.tech - 10) / 2) > 0 ? "+"+Math.floor((this.props.tech - 10) / 2): 0}</h2></h4>
                 <h4 className="skills-list">Medic<h2 className="skills-list-numbers">{Math.floor((this.props.int - 10) / 2) > 0 ? "+"+Math.floor((this.props.int - 10) / 2): 0}</h2></h4>
                 <h4 className="skills-list">Repairs<h2 className="skills-list-numbers">{Math.floor((this.props.tech - 10) / 2) > 0 ? "+"+Math.floor((this.props.tech - 10) / 2): 0}</h2></h4>
-                <h4 className="skills-list">Resistence<h2 className="skills-list-numbers">{Math.floor((this.props.dex - 10) / 2) > 0 ? "+"+Math.floor((this.props.dex - 10) / 2): 0}</h2></h4>
+                <h4 className="skills-list">Resistence<h2 className="skills-list-numbers">{Math.floor((this.props.mat - 10) / 2) > 0 ? "+"+Math.floor((this.props.mat - 10) / 2): 0}</h2></h4>
                 <h4 className="skills-list">Perception<h2 className="skills-list-numbers">{Math.floor((this.props.int - 10) / 2) > 0 ? "+"+Math.floor((this.props.int - 10) / 2): 0}</h2></h4>
                 <h4 className="skills-list">Persuasion<h2 className="skills-list-numbers">{Math.floor((this.props.cha - 10) / 2) > 0 ? "+"+Math.floor((this.props.cha - 10) / 2): 0}</h2></h4>
                 <h4 className="skills-list">SleightOfHand<h2 className="skills-list-numbers">{Math.floor((this.props.dex - 10) / 2) > 0 ? "+"+Math.floor((this.props.dex - 10) / 2): 0}</h2></h4>
